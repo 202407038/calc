8ba2a81e3e2d45764761ab007bffcb059b081704
kgh <k7gam60@gmail.com>
2025-06-05 15:46:50 +0900

    1. dive  [ + ] 곱하기 추가

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -1,5 +1,11 @@
 #include "dive.h"
+#include <iostream>
+using namespace std;
 
 int dive(int x, int y) {
-	return 0;
+        if (y == 0) {
+            cout << "0으로 나눌 수 없습니다.\n";
+            return 0.0;
+        }
+        return (double)x / y;  
 }
